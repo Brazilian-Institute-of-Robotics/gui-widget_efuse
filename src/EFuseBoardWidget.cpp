279a2dfb21a63fa543ba8756017e30563d005262
Rafael Saback <rafael.saback@fieb.org.br>
2015-04-23 11:13:18 +0200

    Added 8 channels with simplified view.

--- src/EFuseBoardWidget.cpp
+++ src/EFuseBoardWidget.cpp
@@ -49,116 +49,102 @@ EFuseBoardWidget::EFuseBoardWidget(QWidget *parent)
 //
 //    QMetaObject::connectSlotsByName(MainWindow);
 
-    resize(600, 1000);
+    resize(520, 600);
 
-//    label = new QLabel(this);
-//    label->setObjectName(QString::fromUtf8("label"));
-//    label->setGeometry(QRect(280, 250, 66, 17));
-//    label->setText()
+//    tab_window = new QTabWidget(this);
+//    tab_window->setGeometry(QRect(0, 0, 100, 25));
+//    tab_window->
 
-    int first_collumn = 10;
-    int second_column = 150;
-//    int second_column = 150;
+    ch1.chan_num = "1";
+    drawChannel(ch1, 300, 450);
 
-    group_box_ch1 = new QGroupBox(this);
-    group_box_ch1->setGeometry(400, 600, 200, 185);
-    group_box_ch1->setStyleSheet("QGroupBox{border: 2px solid;}");
+    ch2.chan_num = "2";
+    drawChannel(ch2, 300, 300);
 
-    /* CHANNEL LABEL */
+    ch3.chan_num = "3";
+    drawChannel(ch3, 300, 150);
 
-    label_ch1 = new QLabel("CH 1",group_box_ch1);
-    label_ch1->setObjectName(QString::fromUtf8("label_ch1"));
-    label_ch1->setFont(QFont("Verdana", 10, 100));
-    label_ch1->setGeometry(QRect(first_collumn, 10, 100, 25));
-    label_ch1->setAlignment(Qt::AlignHCenter);
+    ch4.chan_num = "4";
+    drawChannel(ch4, 300, 10);
 
-    /* CHANNEL STATUS */
+    ch5.chan_num = "5";
+    drawChannel(ch5, 10, 450);
 
-    ch1_power = new QLabel("",group_box_ch1);
-    ch1_power->setObjectName(QString::fromUtf8("ch1_power"));
-    ch1_power->setAlignment(Qt::AlignHCenter);
-    ch1_power->setGeometry(QRect(second_column-10, 10, 50, 15));
-    ch1_power->setStyleSheet("background-color: red;");
+    ch6.chan_num = "6";
+    drawChannel(ch6, 10, 300);
 
-    /* MAX CURRENT LABEL */
+    ch7.chan_num = "7";
+    drawChannel(ch7, 10, 150);
 
-    label_ch1_max_curr = new QLabel("MC",group_box_ch1);
-    label_ch1_max_curr->setObjectName(QString::fromUtf8("label_ch1_max_current"));
-    label_ch1_max_curr->setFont(QFont("Verdana", 10));
-    label_ch1_max_curr->setGeometry(QRect(first_collumn, 50, 140, 25));
-    label_ch1_max_curr->setAlignment(Qt::AlignLeft);
+    ch8.chan_num = "8";
+    drawChannel(ch8, 10, 10);
 
-    /* MAX CURRENT VALUE */
 
-    ch1_max_curr = new QLabel("0",group_box_ch1);
-    ch1_max_curr->setObjectName(QString::fromUtf8("ch1_max_current"));
-    ch1_max_curr->setFont(QFont("Verdana", 10));
-    ch1_max_curr->setGeometry(QRect(second_column, 50, 30, 25));
-    ch1_max_curr->setAlignment(Qt::AlignHCenter);
 
-    /* ACTUAL CURRENT LABEL */
-
-    label_ch1_actual_curr = new QLabel("AC",group_box_ch1);
-    label_ch1_actual_curr->setObjectName(QString::fromUtf8("label_ch1_actual_curr"));
-    label_ch1_actual_curr->setFont(QFont("Verdana", 10));
-    label_ch1_actual_curr->setGeometry(QRect(first_collumn, 75, 140, 25));
-    label_ch1_actual_curr->setAlignment(Qt::AlignLeft);
-
-    /* ACTUAL CURRENT VALUE */
-
-    ch1_actual_curr = new QLabel("0",group_box_ch1);
-    ch1_actual_curr->setObjectName(QString::fromUtf8("ch1_actual_curr"));
-    ch1_actual_curr->setFont(QFont("Verdana", 10));
-    ch1_actual_curr->setGeometry(QRect(second_column, 75, 30, 25));
-    ch1_actual_curr->setAlignment(Qt::AlignHCenter);
-
-    /* POWER UP LABEL */
-
-    label_ch1_power_up = new QLabel("PUP",group_box_ch1);
-    label_ch1_power_up->setObjectName(QString::fromUtf8("label_ch1_power_up"));
-    label_ch1_power_up->setFont(QFont("Verdana", 10));
-    label_ch1_power_up->setGeometry(QRect(first_collumn, 100, 140, 25));
-    label_ch1_power_up->setAlignment(Qt::AlignLeft);
-
-    /* POWER UP STATUS */
-
-    ch1_power_up = new QLabel("",group_box_ch1);
-    ch1_power_up->setObjectName(QString::fromUtf8("ch1_power_up"));
-    ch1_power_up->setGeometry(QRect(second_column+10, 100, 20, 10));
-    ch1_power_up->setAlignment(Qt::AlignHCenter);
-    ch1_power_up->setStyleSheet("background-color: red;");
-
-    /* VOLTAGE LABEL */
-
-    label_ch1_check_voltage = new QLabel("V",group_box_ch1);
-    label_ch1_check_voltage->setObjectName(QString::fromUtf8("label_ch1_check_voltage"));
-    label_ch1_check_voltage->setFont(QFont("Verdana", 10));
-    label_ch1_check_voltage->setGeometry(QRect(first_collumn, 125, 140, 25));
-    label_ch1_check_voltage->setAlignment(Qt::AlignLeft);
-
-    /* VOLTAGE STATUS */
-
-    ch1_check_voltage = new QLabel("",group_box_ch1);
-    ch1_check_voltage->setObjectName(QString::fromUtf8("ch1_check_voltage"));
-    ch1_check_voltage->setGeometry(QRect(second_column+10, 125, 20, 10));
-    ch1_check_voltage->setAlignment(Qt::AlignHCenter);
-    ch1_check_voltage->setStyleSheet("background-color: red;");
-
-    /* LED LABEL */
-
-    label_ch1_check_led = new QLabel("LED:",group_box_ch1);
-    label_ch1_check_led->setObjectName(QString::fromUtf8("label_ch1_check_led"));
-    label_ch1_check_led->setFont(QFont("Verdana", 10));
-    label_ch1_check_led->setGeometry(QRect(first_collumn, 150, 140, 25));
-    label_ch1_check_led->setAlignment(Qt::AlignLeft);
-
-    /* LED STATUS */
-
-    ch1_check_led = new QLabel("",group_box_ch1);
-    ch1_check_led->setObjectName(QString::fromUtf8("ch1_check_led"));
-    ch1_check_led->setGeometry(QRect(second_column+10, 150, 20, 10));
-    ch1_check_led->setAlignment(Qt::AlignHCenter);
-    ch1_check_led->setStyleSheet("background-color: red;");
+//    /* MAX CURRENT LABEL */
+//
+//    label_ch1_max_curr = new QLabel("MC",group_box_ch1);
+//    label_ch1_max_curr->setObjectName(QString::fromUtf8("label_ch1_max_current"));
+//    label_ch1_max_curr->setFont(QFont("Verdana", 10));
+//    label_ch1_max_curr->setGeometry(QRect(first_collumn, 50, 140, 25));
+//    label_ch1_max_curr->setAlignment(Qt::AlignLeft);
+//
+//    /* MAX CURRENT VALUE */
+//
+//    ch1_max_curr = new QLabel("0",group_box_ch1);
+//    ch1_max_curr->setObjectName(QString::fromUtf8("ch1_max_current"));
+//    ch1_max_curr->setFont(QFont("Verdana", 10));
+//    ch1_max_curr->setGeometry(QRect(second_column, 50, 30, 25));
+//    ch1_max_curr->setAlignment(Qt::AlignHCenter);
+//
+//
+//    /* POWER UP LABEL */
+//
+//    label_ch1_power_up = new QLabel("PUP",group_box_ch1);
+//    label_ch1_power_up->setObjectName(QString::fromUtf8("label_ch1_power_up"));
+//    label_ch1_power_up->setFont(QFont("Verdana", 10));
+//    label_ch1_power_up->setGeometry(QRect(first_collumn, 100, 140, 25));
+//    label_ch1_power_up->setAlignment(Qt::AlignLeft);
+//
+//    /* POWER UP STATUS */
+//
+//    ch1_power_up = new QLabel("",group_box_ch1);
+//    ch1_power_up->setObjectName(QString::fromUtf8("ch1_power_up"));
+//    ch1_power_up->setGeometry(QRect(second_column+10, 100, 20, 10));
+//    ch1_power_up->setAlignment(Qt::AlignHCenter);
+//    ch1_power_up->setStyleSheet("background-color: red;");
+//
+//    /* VOLTAGE LABEL */
+//
+//    label_ch1_check_voltage = new QLabel("V",group_box_ch1);
+//    label_ch1_check_voltage->setObjectName(QString::fromUtf8("label_ch1_check_voltage"));
+//    label_ch1_check_voltage->setFont(QFont("Verdana", 10));
+//    label_ch1_check_voltage->setGeometry(QRect(first_collumn, 125, 140, 25));
+//    label_ch1_check_voltage->setAlignment(Qt::AlignLeft);
+//
+//    /* VOLTAGE STATUS */
+//
+//    ch1_check_voltage = new QLabel("",group_box_ch1);
+//    ch1_check_voltage->setObjectName(QString::fromUtf8("ch1_check_voltage"));
+//    ch1_check_voltage->setGeometry(QRect(second_column+10, 125, 20, 10));
+//    ch1_check_voltage->setAlignment(Qt::AlignHCenter);
+//    ch1_check_voltage->setStyleSheet("background-color: red;");
+//
+//    /* LED LABEL */
+//
+//    label_ch1_check_led = new QLabel("LED:",group_box_ch1);
+//    label_ch1_check_led->setObjectName(QString::fromUtf8("label_ch1_check_led"));
+//    label_ch1_check_led->setFont(QFont("Verdana", 10));
+//    label_ch1_check_led->setGeometry(QRect(first_collumn, 150, 140, 25));
+//    label_ch1_check_led->setAlignment(Qt::AlignLeft);
+//
+//    /* LED STATUS */
+//
+//    ch1_check_led = new QLabel("",group_box_ch1);
+//    ch1_check_led->setObjectName(QString::fromUtf8("ch1_check_led"));
+//    ch1_check_led->setGeometry(QRect(second_column+10, 150, 20, 10));
+//    ch1_check_led->setAlignment(Qt::AlignHCenter);
+//    ch1_check_led->setStyleSheet("background-color: red;");
 
 
 
@@ -173,9 +159,62 @@ EFuseBoardWidget::EFuseBoardWidget(QWidget *parent)
 
 }
 
-void EFuseBoardWidget::drawMonitoringTab(QGroupBox *group_box, QLabel *power, QLabel *actual_current,
-		 	 	 	   QLabel *max_current, int x_pos, int y_pos)
+void EFuseBoardWidget::drawChannel(ChannelGroupBox &channel, int x_pos, int y_pos)
 {
+
+    int first_collumn = 10;
+    int second_column = 150;
+    int first_row = 10;
+    int second_row = 55;
+    int third_row = 85;
+    channel.max_current = 10.5;
+
+	channel.group_box = new QGroupBox(this);
+	channel.group_box->setGeometry(x_pos, y_pos, 200, 120);
+	channel.group_box->setStyleSheet("QGroupBox{border: 2px solid;}");
+
+    /* CHANNEL LABEL */
+
+	channel.label_channel = new QLabel("CH "+channel.chan_num,channel.group_box);
+	channel.label_channel->setObjectName("label_ch"+channel.chan_num);
+	channel.label_channel->setFont(QFont("Verdana", 10, 100));
+	channel.label_channel->setGeometry(QRect(first_collumn, first_row, 100, 25));
+	channel.label_channel->setAlignment(Qt::AlignHCenter);
+
+    /* CHANNEL STATUS */
+
+	channel.status_channel = new QLabel("",channel.group_box);
+	channel.status_channel->setObjectName("ch"+channel.chan_num+"_power");
+	channel.status_channel->setAlignment(Qt::AlignHCenter);
+	channel.status_channel->setGeometry(QRect(second_column-10, first_row, 50, 15));
+	channel.status_channel->setStyleSheet("background-color: red;");
+
+	/* ACTUAL CURRENT LABEL */
+
+	channel.label_current = new QLabel("Actual Current",channel.group_box);
+	channel.label_current->setObjectName("label_ch"+channel.chan_num+"_actual_curr");
+	channel.label_current->setFont(QFont("Verdana", 10));
+	channel.label_current->setGeometry(QRect(first_collumn, second_row, 140, 25));
+	channel.label_current->setAlignment(Qt::AlignLeft);
+
+	/* ACTUAL CURRENT VALUE */
+
+	channel.value_current = new QLabel("0",channel.group_box);
+	channel.value_current->setObjectName("ch"+channel.chan_num+"_actual_curr");
+	channel.value_current->setFont(QFont("Verdana", 10));
+	channel.value_current->setGeometry(QRect(second_column, second_row, 30, 25));
+	channel.value_current->setAlignment(Qt::AlignHCenter);
+
+	/* PROGRESS BAR CURRENT */
+
+	channel.progress_current = new QProgressBar(channel.group_box);
+	channel.progress_current->setMaximum(channel.max_current);
+	channel.progress_current->setTextVisible(true);
+	channel.progress_current->setValue(0);
+	channel.progress_current->setFormat("%p%");
+	channel.progress_current->setObjectName("ch"+channel.chan_num+"_prog_curr");
+	channel.progress_current->setGeometry(QRect(first_collumn, third_row, 180, 20));
+	channel.progress_current->setAlignment(Qt::AlignHCenter);
 
 }
 
--- src/EFuseBoardWidget.hpp
+++ src/EFuseBoardWidget.hpp
@@ -3,14 +3,26 @@
 
 #include <QtGui>
 
+struct ChannelGroupBox
+{
+	QGroupBox 		*group_box;
+	QLabel 			*label_channel;
+	QLabel 			*status_channel;
+	QLabel 			*label_current;
+	QLabel 			*value_current;
+	QProgressBar 	*progress_current;
+	QString  	 	 chan_num;
+	int				 max_current;
+};
+
 class EFuseBoardWidget : public QWidget
 {
     Q_OBJECT
 public:
     EFuseBoardWidget(QWidget *parent = 0);
     virtual ~EFuseBoardWidget();
-    void drawMonitoringTab(QGroupBox *group_box, QLabel *power, QLabel *actual_current,
-			 	 	 	   QLabel *max_current, int x_pos, int y_pos);
+    void drawMonitoringTab();
+    void drawChannel(ChannelGroupBox &channel, int x_pos, int y_pos);
     void drawConfigurationTab();
 
 private:
@@ -20,23 +32,37 @@ private:
 //    QStatusBar *statusbar;
 //    QFrame *frame;
 //    QGroupBox *group_box_ch1;
-    QLabel *label_ch1;
-    QLabel *label_ch1_max_curr;
-    QLabel *label_ch1_actual_curr;
-    QLabel *label_ch1_power_up;
-    QLabel *label_ch1_check_voltage;
-    QLabel *label_ch1_check_led;
+//    QLabel *label_ch1;
+//    QLabel *label_ch1_max_curr;
+//    QLabel *label_ch1_actual_curr;
+//    QLabel *label_ch1_power_up;
+//    QLabel *label_ch1_check_voltage;
+//    QLabel *label_ch1_check_led;
+//
+//    QLabel *ch1_power;
+//    QLabel *ch1_max_curr;
+//    QLabel *ch1_actual_curr;
+//    QLabel *ch1_power_up;
+//    QLabel *ch1_check_voltage;
+//    QLabel *ch1_check_led;
 
-    QLabel *ch1_power;
-    QLabel *ch1_max_curr;
-    QLabel *ch1_actual_curr;
-    QLabel *ch1_power_up;
-    QLabel *ch1_check_voltage;
-    QLabel *ch1_check_led;
+    ChannelGroupBox ch1;
+    ChannelGroupBox ch2;
+    ChannelGroupBox ch3;
+    ChannelGroupBox ch4;
+    ChannelGroupBox ch5;
+    ChannelGroupBox ch6;
+    ChannelGroupBox ch7;
+    ChannelGroupBox ch8;
 
-    QGroupBox *group_box_ch1;
-    QFormLayout *flay;
-    QFormLayout *flay2;
+    QTabWidget *tab_window;
+    QWidget *config_tab;
+    QWidget *monit_tab;
+
+
+//    QGroupBox *group_box_ch1;
+//    QFormLayout *flay;
+//    QFormLayout *flay2;
 };
 
 #endif /* EFUSEBOARDWIDGET_HPP */
